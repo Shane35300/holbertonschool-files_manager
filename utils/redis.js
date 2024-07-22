7165ccf7b3d7d7745f8c1a70abf07db0a2cf4931
shane <6874@holbertonstudents.com>
2024-07-22 16:38:20 +0200

    task 0

---
+++ utils/redis.js
@@ -0,0 +1,42 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+	constructor() {
+		this.client = redis.createClient();
+		this.connected = false;
+
+		// Handle connection errors
+		this.client.on('error', (err) => {
+			console.error(`Redis client not connected to the server: ${err}`);
+		});
+		this.client.on('connect', () => {
+			this.connected = true;
+		});
+
+		// Promisify the get, set, and del functions for async/await usage
+		this.getAsync = promisify(this.client.get).bind(this.client);
+		this.setAsync = promisify(this.client.set).bind(this.client);
+		this.delAsync = promisify(this.client.del).bind(this.client);
+	}
+
+	isAlive() {
+		return this.connected;
+	}
+
+	async get(key) {
+		return await this.getAsync(key);
+	}
+
+	async set(key, value, duration) {
+		await this.setAsync(key, value, 'EX', duration);
+	}
+
+	async del(key) {
+		await this.delAsync(key);
+	}
+}
+
+// Create and export an instance of RedisClient
+const redisClient = new RedisClient();
+export default redisClient;
